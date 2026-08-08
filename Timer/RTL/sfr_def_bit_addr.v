81b685e769d55a1f176c8cb328a0adeb1c44d3d1
Jay Sanga <96255543+jay-verilog@users.noreply.github.com>
2026-08-08 21:39:21 +0530

    Add files via upload

---
+++ Timer/RTL/sfr_def_bit_addr.v
@@ -0,0 +1,110 @@
+// ACCUMULATION
+`define ACC_7     3'b111
+`define ACC_6     3'b110
+`define ACC_5     3'b101
+`define ACC_4     3'b100
+`define ACC_3     3'b011
+`define ACC_2     3'b010
+`define ACC_1     3'b001
+`define ACC_0     3'b000
+
+// B REGISTER
+`define B_REG_7   3'b111
+`define B_REG_6   3'b110
+`define B_REG_5   3'b101
+`define B_REG_4   3'b100
+`define B_REG_3   3'b011
+`define B_REG_2   3'b010
+`define B_REG_1   3'b001
+`define B_REG_0   3'b000
+
+// PWS
+`define CY     	3'b111
+`define AC     	3'b110
+`define F0     	3'b101
+`define RS1     3'b100
+`define RS0     3'b011
+`define OV     	3'b010
+`define PR      3'b000
+
+//PORT 0
+`define P0_7   	3'b111
+`define P0_6   	3'b110
+`define P0_5   	3'b101
+`define P0_4   	3'b100
+`define P0_3   	3'b011
+`define P0_2   	3'b010
+`define P0_1   	3'b001
+`define P0_0   	3'b000
+
+//PORT 1
+`define P1_7   	3'b111
+`define P1_6   	3'b110
+`define P1_5   	3'b101
+`define P1_4   	3'b100
+`define P1_3   	3'b011
+`define P1_2   	3'b010
+`define P1_1   	3'b001
+`define P1_0   	3'b000
+
+//PORT 2
+`define P2_7   	3'b111
+`define P2_6   	3'b110
+`define P2_5   	3'b101
+`define P2_4   	3'b100
+`define P2_3   	3'b011
+`define P2_2   	3'b010
+`define P2_1   	3'b001
+`define P2_0   	3'b000
+
+//PORT 3
+`define P3_7   	3'b111
+`define P3_6   	3'b110
+`define P3_5   	3'b101
+`define P3_4   	3'b100
+`define P3_3   	3'b011
+`define P3_2   	3'b010
+`define P3_1   	3'b001
+`define P3_0   	3'b000
+
+
+`define PS 	3'b100
+`define PT1	3'b011
+`define PX1	3'b010
+`define PT0	3'b001
+`define PX0	3'b000
+
+`define EA 	3'b111
+`define ES	3'b100
+`define ET1	3'b011
+`define EX1	3'b010
+`define ET0	3'b001
+`define EX0	3'b000
+
+`define TF1	3'b111
+`define TR1 	3'b110
+`define TF0	3'b101
+`define TR0	3'b100
+`define IE1	3'b011
+`define IT1	3'b010
+`define IE0	3'b001
+`define IT0	3'b000
+
+`define SM0     3'b111
+`define SM1	3'b110
+`define SM2	3'b101
+`define REN	3'b100
+`define TB8	3'b011
+`define RB8	3'b010
+`define TI	3'b001
+`define RI 	3'b000
+
+`define GATE1   3'b111
+`define CT1	3'b110
+`define T1M1	3'b101
+`define T1M0	3'b100
+`define GATE0   3'b011
+`define CT0	3'b010
+`define T0M1	3'b001
+`define T0M0	3'b000
+
