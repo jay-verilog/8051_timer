f77a1a1221eda6d77e83eed3fd43fec5988a431d
Jay Sanga <96255543+jay-verilog@users.noreply.github.com>
2026-08-08 21:46:37 +0530

    Update sfr_def_addr.v

--- Timer/RTL/sfr_def_addr.v
+++ Timer/RTL/sfr_def_addr.v
@@ -1,29 +1,30 @@
 `define ACC     8'he0
 `define B_REG   8'hf0
 `define PSW     8'hd0
+
 `define SP      8'h81
+
 `define DPL     8'h82
 `define DPH     8'h83
 
 `define P0      8'h80
-`define P0_DIR  8'h85
 `define P1      8'h90
-`define P1_DIR  8'h95
 `define P2      8'ha0
-`define P2_DIR  8'ha5
 `define P3      8'hb0
-`define P3_DIR  8'hb5
  
 `define IP      8'hb8
 `define IE      8'ha8
+
 `define TMOD    8'h89
 `define TCON    8'h88
 `define TH0     8'h8c
 `define TL0     8'h8a
 `define TH1     8'h8d
 `define TL1     8'h8b
+
 `define SCON    8'h98
 `define SBUF    8'h99
+
 `define PCON    8'h87
 
 
